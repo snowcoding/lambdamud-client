5e3381638ba42274fe46d2eac40f2e5b38d703cb
snowcoding <vimalshah77@gmail.com>
2018-09-25 21:13:21 -0700

    Removed Game state from App.js

--- react-mud-client/src/App.js
+++ react-mud-client/src/App.js
@@ -9,12 +9,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userAuthToken: "",
-      uuid:"",
-      name:"",
-      title:"",
-      description:"",
-      players: []
+      userAuthToken: ""
     };
   }
 
@@ -24,22 +19,12 @@ class App extends Component {
     }
   }
 
-  initPlayer = (init) => {
-    this.setState({
-      uuid:init.uuid,
-      name:init.name,
-      players:init.players,
-      title:init.title,
-      description:init.description
-    })
-  }
-
   render() {
     return (
       <div className="App">
 
         {/* How to send a prop down to component within a route */}
-        <Route exact path="/register" component={(props) => <Register initPlayer={this.initPlayer} {...props}/> } />
+        <Route exact path="/register" component={(props) => <Register {...props}/> } />
         
         
         <Route exact path="/" render={(props) => this.state.userAuthToken ? 
@@ -47,9 +32,10 @@ class App extends Component {
                         <Route path="/adventure" component={Adventure} /> : 
                         <Redirect to="/login" /> }/>
 
-        <Route exact path="/login" component={(props) => <Login initPlayer={this.initPlayer} {...props}/>} />
-        
-        <Route path="/adventure" component={() => <Adventure uuid={this.state.uuid} 
+        <Route exact path="/login" component={(props) => <Login {...props}/>} />
+
+        <Route path="/adventure" component={() => <Adventure userAuthToken={this.state.userAuthToken}
+                                                            uuid={this.state.uuid} 
                                                             name={this.state.name} 
                                                             players={this.state.players}
                                                             title={this.state.title}

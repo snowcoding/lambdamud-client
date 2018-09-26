b0a3212175ad1877abda6ed56cecb47e5769724e
snowcoding <vimalshah77@gmail.com>
2018-09-26 10:15:34 -0700

    Message History functional
    - Capture messages
    - Update message history (textArea)
    - clean up UI

--- react-mud-client/src/components/Adventure.js
+++ react-mud-client/src/components/Adventure.js
@@ -9,7 +9,9 @@ class Adventure extends Component {
       name:"",
       title:"",
       description:"",
-      players: []
+      players: [],
+      message:"",
+      command:""
     };
   }
 
@@ -22,11 +24,16 @@ class Adventure extends Component {
     axios
       .get(apiBaseUrl + "adv/init/", headerWithUserToken)
       .then(response => {
-        console.log("Initialization successful");
+        console.log("Initialization successful");        
         console.log(response);
+        
         let data = response.data
         console.log(data)
+        
         this.setState({...data})
+
+        this.updateMessageHistory(data.title, data.description)
+
 
       })
       .catch(error => {
@@ -35,14 +42,52 @@ class Adventure extends Component {
       });
   }
 
+  // Grab the command text
+  captureCommand = (e) => {
+    if (e.target.id === 'command'){
+      console.log(e.target.value)
+      
+      // If it's coming from the command then update the command state
+      this.setState({command:e.target.value})
+    }
+  }
+
+  // Check the key of the command input
+  onCommandKeyPress = (e) => {
+
+    // If the user presses enter
+    if (e.charCode == 13) {
+      e.preventDefault();
+      e.stopPropagation();
+      console.log('You clicked enter!!')
+
+      this.updateMessageHistory('this just in', e.target.value)
+      e.target.value = ''
+
+    }
+  }
+
+  //Update the message state with any incoming messages
+  updateMessageHistory = (title, description) => {
+    this.setState({
+      message: `${this.state.message} \n ${title} \n ${description} \n`
+    }, ()=> {
+      let messageHistory = document.getElementById('messageHistory')
+      messageHistory.scrollTop = messageHistory.scrollHeight
+    })
+  }
+
   render() {
     return (
       <div>
-        WELCOME TO THE ADVENTURE!!!
-        <p>UUID: {this.state.uuid}</p>
-        <p>Name: {this.state.name}</p>
-        <p>Title: {this.state.title}</p>
-        <p>Description: {this.state.description}</p>
+        <p>WELCOME TO THE ADVENTURE!!!</p>
+
+        <textarea rows="10" id="messageHistory" readOnly value={this.state.message}/>
+        {/* <textarea id="messageHistory" defaultValue={this.state.message}/> */}
+        <div>
+          <input type="text" id="command" onChange={this.captureCommand} onKeyPress={this.onCommandKeyPress}/>
+          <button>Send</button>
+        </div>
       </div>
     );
   }

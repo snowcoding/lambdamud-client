3eb9b9e2b33727f3388240861da012244d3b5bbe
snowcoding <vimalshah77@gmail.com>
2018-09-26 10:07:54 -0700

    Clean up
    - Remove state
    - removed componentWillMount
    - remove sending state to Adv
    - added authToken check for /adventure

--- react-mud-client/src/App.js
+++ react-mud-client/src/App.js
@@ -6,40 +6,27 @@ import Adventure from "./components/Adventure";
 import Register from "./components/Register"
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userAuthToken: ""
-    };
-  }
 
-  componentWillMount() {
-    if (localStorage.getItem("mudToken")) {
-      this.setState({ userAuthToken: localStorage.getItem("mudToken") });
-    }
+  getUserAuthToken = () => {
+    return localStorage.getItem('mudToken')
   }
 
   render() {
     return (
       <div className="App">
-
-        {/* How to send a prop down to component within a route */}
+        {/* Send a prop down to component within a route */}
         <Route exact path="/register" component={(props) => <Register {...props}/> } />
         
         
-        <Route exact path="/" render={(props) => this.state.userAuthToken ? 
-                        // <Redirect to="/adventure" /> : 
+        <Route exact path="/" render={(props) => this.getUserAuthToken() ? 
                         <Route path="/adventure" component={Adventure} /> : 
                         <Redirect to="/login" /> }/>
 
         <Route exact path="/login" component={(props) => <Login {...props}/>} />
 
-        <Route path="/adventure" component={() => <Adventure userAuthToken={this.state.userAuthToken}
-                                                            uuid={this.state.uuid} 
-                                                            name={this.state.name} 
-                                                            players={this.state.players}
-                                                            title={this.state.title}
-                                                            desc={this.state.description}/>} />
+        <Route path="/adventure" render={(props) => this.getUserAuthToken() ? 
+                            <Adventure /> :
+                            <Redirect to="/login" /> }/>
         
       </div>
     );

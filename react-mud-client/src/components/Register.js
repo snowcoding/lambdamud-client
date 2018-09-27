87b7c9de04638a4e53885e13618c32346d9a4315
snowcoding <vimalshah77@gmail.com>
2018-09-27 13:32:21 -0700

    Style register page

--- react-mud-client/src/components/Register.js
+++ react-mud-client/src/components/Register.js
@@ -1,5 +1,20 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import styled from 'styled-components';
+import { Container, Row, Col, Button } from 'reactstrap';
+
+const Label = styled.label`
+  color:#565356;
+  text-align:right;
+`
+
+const LoginRow = styled(Row)`
+  margin-top:20px;
+  margin-bottom:40px;
+`
+const UsernameRow = styled(Row)`
+  margin-top:20px;
+`
 
 class Register extends Component {
   constructor(props) {
@@ -53,21 +68,56 @@ class Register extends Component {
 
   render() {
     return (
-      <div>
-        <p>
-          <label htmlFor="username" className="username">Username:</label>
-          <input id='username' type="text" className="username" onChange={this.onFieldChange}/>          
-        </p>
-        <p>
-          <label htmlFor="password1" className="password1">Password:</label>
-          <input id='password1' type="password" className="password1" onChange={this.onFieldChange}/>
-        </p>
-        <p>
-          <label htmlFor="password2" className="password2">Password (Retype):</label>
+      <Container>
+      <UsernameRow>
+        <Col xs="5"></Col>
+        <Col xs="1">
+          <Label htmlFor="username" className="username">Username:</Label>
+        </Col>
+        <Col xs="6">
+          <input id="username" type="text" className="username" onChange={this.onFieldChange}/>
+        </Col>
+      </UsernameRow>
+      <Row>
+        <Col xs="5"></Col>
+        <Col xs="1">
+        <Label htmlFor="password" className="password">Password:</Label>
+        </Col>
+        <Col xs="6">
+        <input id="password" type="password" className="password" onChange={this.onFieldChange}/>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs="5"></Col>
+        <Col xs="1">
+          <Label htmlFor="password2" className="password2">Password (Retype):</Label>
+        </Col>
+        <Col xs="6">
           <input id='password2' type="password" className="password2" onChange={this.onFieldChange}/>
-        </p>
-        <button onClick = {this.onRegister}>Register</button>
-      </div>
+        </Col>
+      </Row>
+      <LoginRow>
+        <Col xs="7"></Col>
+        <Col xs="2"><Button onClick = {this.onRegister}>Register</Button></Col>
+        <Col xs="3"></Col>
+      </LoginRow>      
+    </Container>
+
+      // <div>
+      //   <p>
+      //     <label htmlFor="username" className="username">Username:</label>
+      //     <input id='username' type="text" className="username" onChange={this.onFieldChange}/>          
+      //   </p>
+      //   <p>
+      //     <label htmlFor="password1" className="password1">Password:</label>
+      //     <input id='password1' type="password" className="password1" onChange={this.onFieldChange}/>
+      //   </p>
+      //   <p>
+      //     <label htmlFor="password2" className="password2">Password (Retype):</label>
+      //     <input id='password2' type="password" className="password2" onChange={this.onFieldChange}/>
+      //   </p>
+      //   <button onClick = {this.onRegister}>Register</button>
+      // </div>
     );
   }
 }
